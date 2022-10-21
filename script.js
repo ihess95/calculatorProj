070df03b1fde69da915e55f0b1ae91cdbe3a52e8
Ian Hess <ian.h.hess@gmail.com>
2022-10-21 12:54:00 -0400

    create and position ops buttons create misc

--- script.js
+++ script.js
@@ -14,18 +14,50 @@ function makeNumbers() {
       row.className = "numButton";
       col.appendChild(row);
     }
-    numbersContainer.appendChild(col);
+    numbersContainer.insertBefore(col, numbersContainer.misc);
   }
 }
 
-// function makeOps(){
-//     for (let i = 0; i < 4)
-// }
+function makeOps() {
+  for (let i = 0; i < 1; i++) {
+    let col = document.createElement("div");
+    col.className = "opsCol";
+    for (let j = 0; j < 4; j++) {
+      let row = document.createElement("div");
+      row.className = "opsButton";
+      col.appendChild(row);
+    }
+    opsContainer.appendChild(col);
+  }
+}
+
+function makeMisc() {
+  for (let i = 0; i < 1; i++) {
+    let col = document.createElement("div");
+    col.className = "miscCol";
+    for (let j = 0; j < 4; j++) {
+      let row = document.createElement("div");
+      row.className = "miscButton";
+      col.appendChild(row);
+    }
+    miscContainer.appendChild(col);
+  }
+}
 
 makeNumbers();
+makeOps();
+makeMisc();
 
 const numButtons = document.querySelectorAll(".numButton");
 numButtons.forEach((button) => {
   button.textContent = temp;
   temp++;
+});
+const opsButton = document.querySelectorAll(".opsButton");
+opsButton.forEach((button) => {
+  button.textContent = "test";
+});
+const miscButton = document.querySelectorAll(".miscButton");
+miscButton.forEach((button) => {
+  button.textContent = "misc";
 });
